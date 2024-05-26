044247b3053a9db351167a86db8204af1b6ac6ea
Arunpatwa123 <143103768+Arunpatwa123@users.noreply.github.com>
2024-05-26 19:09:42 +0530

    new files

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,8 +1,7 @@
 import React,{useState} from 'react'
 
-
 export default function TextForm(props) {
-  const [text, setText]=useState();
+  const [text, setText]=useState("Enter Your Text");
   const handleUpClick=()=>{
     // console.log('Upper case was clicked'+text);
     let newText=text.toUpperCase()
@@ -34,8 +33,9 @@ export default function TextForm(props) {
 
   return (
     <>
-  <div className="mb-3">
 
+  <div className="container">
+  <div className="mb-3">
     <h1>{props.heading} </h1>
     <div className='mb-3'>
     <textarea className="form-control" value={text} id="mybox"  placeholder='Enter Your text' onChange={handleOnChange} rows="10"></textarea>
@@ -45,6 +45,14 @@ export default function TextForm(props) {
      <button className="btn btn-primary" onClick={handleToReverse}>Reverse</button>
     <button className="btn btn-primary" onClick={handleToRemoveToSpace}>Remove space</button> 
     <button className="btn btn-primary" onClick={Cancel}>Cancel</button> 
+  </div>
+  </div>
+  <div className="container">
+    <h2>Your Text Summary</h2>
+    <p> <strong> {text.split(" ").length}</strong> words and ,<strong>{text.length}</strong> Character</p>
+    <p>{0.008*text.split(" ").length} minutes to read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
   </div>
     </>
   )
